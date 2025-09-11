c3092765aaeebbd0aaf4f3f79bbc9d4ec809bc0e
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2025-09-11 17:22:27 +0100

    Add placeholder message

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <!-- Placeholder to prevent 404s before I add error pages -->
+    <UAlert
+      color="info"
+      class="m-4"
+      title="Page Not Found"
+      description="This is a placeholder page. The content you are looking for may not exist yet."
+    />
   </div>
 </template>
 
