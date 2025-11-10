b8daa4d024985956a7ff6187753f2a3de32e0c8d
MattA-Official <43476212+MattA-Official@users.noreply.github.com>
2025-11-10 01:42:20 +0000

    Deprecate unused fields in Performance model and update migration scripts

---
+++ prisma/migrations/20251110012524_remove_runtime_default/migration.sql
@@ -0,0 +1,27 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Performance" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "startDateTime" DATETIME NOT NULL,
+    "runtimeMinutes" INTEGER NOT NULL,
+    "intervalMinutes" INTEGER NOT NULL DEFAULT 0,
+    "type" TEXT NOT NULL,
+    "details" TEXT,
+    "maxCapacity" INTEGER NOT NULL,
+    "reservationInstructions" TEXT,
+    "externalBookingLink" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "showId" TEXT,
+    "venueId" TEXT,
+    CONSTRAINT "Performance_showId_fkey" FOREIGN KEY ("showId") REFERENCES "Show" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Performance_venueId_fkey" FOREIGN KEY ("venueId") REFERENCES "Venue" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Performance" ("createdAt", "details", "externalBookingLink", "id", "intervalMinutes", "isActive", "maxCapacity", "reservationInstructions", "runtimeMinutes", "showId", "startDateTime", "title", "type", "updatedAt", "venueId") SELECT "createdAt", "details", "externalBookingLink", "id", "intervalMinutes", "isActive", "maxCapacity", "reservationInstructions", "runtimeMinutes", "showId", "startDateTime", "title", "type", "updatedAt", "venueId" FROM "Performance";
+DROP TABLE "Performance";
+ALTER TABLE "new_Performance" RENAME TO "Performance";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
